d135d562e364e5b38a625f8e5b738c55d7b94e8f
Jessica Ramos Molina <jessicaramosmolina@Jessicas-Air.hsd1.ca.comcast.net>
2021-02-20 11:07:37 -0800

    initial commit

--- Terrorism_data/app.js
+++ Terrorism_data/app.js
@@ -1,48 +1,82 @@
-function makeResponsive() {
-    //Define SVG area
-    var svgWidth = 1800;
-    var svgHeight = 900;
+// Read json file
+d3.json("files/countryList.json").then((countryList) => {
+    // console.log(countryList);
+    var countryName = countryList.map(d => d.country);
+    // console.log(countryName);
 
-    var chartMargin = {
-        top: 20,
-        right: 20,
-        bottom: 30,
-        left: 40    
-    };
+    var selectOption = d3.select("body")
+                        .append("div")
+                        .append("select");
 
-    // Define dimensions of the chart area
-    var width = svgWidth - chartMargin.left - chartMargin.right;
-    var height = svgHeight - chartMargin.top - chartMargin.bottom;
-
-    // Select body, append SVG area, and set the dimensions
-    var svg = d3
-    .select("body")
-    .append("svg")
-    .attr("height", svgHeight)
-    .attr("width", svgWidth);
-
-    // Append group element
-    var chartGroup = svg
-    .append("g")
-    .attr("transform", "translate(" + chartMargin.left + "," + chartMargin.top + ")")
-    .attr("class","main");
-
-    //Read CSV
-
-    d3.json("files/terrorismAttacks.json"). then(function(terrorismData) {
-        console.log(terrorismData);
-
-        //parse data
-        terrorismData.forEach(function(data) {
-            data.year = +data.year;
-            data.attacksCount = +data.attacksCount
-            // data.country = data.country.length;
-            // console.log(data);
-
-        });
-
+    countryName.forEach((x) => {
+        selectOption.append("option")
+                    .text(x)
+                    .property("value", x);
     });
-}
+
+    // var selectOption = d3.select("body")
+    //                 .append("div")
+    //                 .append("select");
+
+    // countryName.forEach((x) => {
+    //     selectOption.append("option")
+    //                 .text(x)
+    //                 .property("value", x);
+
+});
+
+// var countrySelected = countryList.country[0];
+// console.log(countrySelected);
+
+
+
+
+
+// function makeResponsive() {
+//     //Define SVG area
+//     var svgWidth = 1800;
+//     var svgHeight = 900;
+
+//     var chartMargin = {
+//         top: 20,
+//         right: 20,
+//         bottom: 30,
+//         left: 40    
+//     };
+
+//     // Define dimensions of the chart area
+//     var width = svgWidth - chartMargin.left - chartMargin.right;
+//     var height = svgHeight - chartMargin.top - chartMargin.bottom;
+
+//     // Select body, append SVG area, and set the dimensions
+//     var svg = d3
+//     .select("body")
+//     .append("svg")
+//     .attr("height", svgHeight)
+//     .attr("width", svgWidth);
+
+//     // Append group element
+//     var chartGroup = svg
+//     .append("g")
+//     .attr("transform", "translate(" + chartMargin.left + "," + chartMargin.top + ")")
+//     .attr("class","main");
+
+//     //Read CSV
+
+//     d3.json("files/terrorismAttacks.json"). then(function(terrorismData) {
+//         console.log(terrorismData);
+
+//         //parse data
+//         terrorismData.forEach(function(data) {
+//             data.year = +data.year;
+//             data.attacksCount = +data.attacksCount
+//             // data.country = data.country.length;
+//             // console.log(data);
+
+//         });
+
+//     });
+// }
 
 
 // d3.csv("test.csv").then((data) => {
@@ -83,8 +117,6 @@ function makeResponsive() {
 
 
 // // });
-
-
 
 
 
