37b7768ac3710c00b9568bc1ecb25bee4b1be7e4
Jessica Ramos Molina <jessicaramosmolina@Jessicas-Air.hsd1.ca.comcast.net>
2021-02-22 17:30:09 -0800

    added dropdown menu and 2 graphs

--- Terrorism_data/app.js
+++ Terrorism_data/app.js
@@ -1,12 +1,12 @@
 // Read json file
 d3.json("files/countryList.json").then((countryList) => {
     // console.log(countryList);
-    var countryName = countryList.map(d => d.country);
+    var countryName = countryList.map(d => d.country_names);
     // console.log(countryName);
 
-    var selectOption = d3.select("body")
-                        .append("div")
-                        .append("select");
+    var selectOption = d3.select("#selDataset");
+                        // .append("div")
+                        // .append("select");
 
     countryName.forEach((x) => {
         selectOption.append("option")
@@ -14,109 +14,84 @@ d3.json("files/countryList.json").then((countryList) => {
                     .property("value", x);
     });
 
-    // var selectOption = d3.select("body")
-    //                 .append("div")
-    //                 .append("select");
-
-    // countryName.forEach((x) => {
-    //     selectOption.append("option")
-    //                 .text(x)
-    //                 .property("value", x);
 
 });
+d3.select('#selDataset')
+  .on('change', function() {
+    console.log(d3.select(this).property("value"));
+    optionChange(d3.select(this).property("value"));
+});
 
-// var countrySelected = countryList.country[0];
-// console.log(countrySelected);
+function optionChange(country_name){
+    d3.json("files/terrorismAttacks.json").then((data) => {
+        data = data.filter(d => d.country===country_name);
+        // console.log(data);
 
+        if (data.find(d => d.year===1990)=== undefined ){
+            data.unshift({year: 1990, attacksCount: null});
+        }
+        if (data.find(d => d.year===2018)=== undefined ){
+            data.push({year: 2018, attacksCount: null});
+        }
 
-
-
-
-// function makeResponsive() {
-//     //Define SVG area
-//     var svgWidth = 1800;
-//     var svgHeight = 900;
-
-//     var chartMargin = {
-//         top: 20,
-//         right: 20,
-//         bottom: 30,
-//         left: 40    
-//     };
-
-//     // Define dimensions of the chart area
-//     var width = svgWidth - chartMargin.left - chartMargin.right;
-//     var height = svgHeight - chartMargin.top - chartMargin.bottom;
-
-//     // Select body, append SVG area, and set the dimensions
-//     var svg = d3
-//     .select("body")
-//     .append("svg")
-//     .attr("height", svgHeight)
-//     .attr("width", svgWidth);
-
-//     // Append group element
-//     var chartGroup = svg
-//     .append("g")
-//     .attr("transform", "translate(" + chartMargin.left + "," + chartMargin.top + ")")
-//     .attr("class","main");
-
-//     //Read CSV
-
-//     d3.json("files/terrorismAttacks.json"). then(function(terrorismData) {
-//         console.log(terrorismData);
-
-//         //parse data
-//         terrorismData.forEach(function(data) {
-//             data.year = +data.year;
-//             data.attacksCount = +data.attacksCount
-//             // data.country = data.country.length;
-//             // console.log(data);
-
-//         });
-
-//     });
-// }
-
-
-// d3.csv("test.csv").then((data) => {
-//     console.log(data)
-// });
-
-// // d3.csv("test.csv").then((data) => {
-// //     // console.log(data);
-// //     var countryName = data.map(d => d.country);
-// //     // console.log(countryName);
-
-// //     var selectOption = d3.select("body")
-// //                     .append("div")
-// //                     .append("select");
-
-// //     countryName.forEach((x) => {
-// //         selectOption.append("option")
-// //                     .text(x)
-// //                     .property("value", x);
-// //     });
-
-
-// //     // CREATE DROPDOWN MENU AND APPEND COUNTRY NAME
+        document.getElementById("plot").innerHTML = "";
+        var trace1 = {
+            x: data.map(d=>d.year),
+            y: data.map(d=>d.attacksCount),
+            // mode: 'markers',
+            type: "line"
+            // name: "Terrorism Attacks per Year"
+        };
     
+        var layout = {
+            title: "Terrorism Attacks per Year",
+            xaxis: {title: "Year"},
+            yaxis: {title: "Number of Attacks"}
+        };
     
-    
-// //     // selectOption.selectAll("option")
-// //     //           .data(data)
-// //     //           .enter()
-// //     //           .append("option")
-// //     //           .attr("value", function (d) {return d; })
-// //     //           .text(function (d) {return d;});
+        Plotly.newPlot("plot", [trace1], layout);
+    });
+/////////////////////////////////////////////////////////
+
+d3.json("files/comparisondata.json").then((data) => {
+    data = data.filter(d => d.country===country_name);
+    var counts = {
+        domestic: 0,
+        international: 0
+    }
+    data.forEach((x) => {
+        if (x.equal){
+            counts.domestic = x.xyz;
+        }
+        if (! x.equal){
+            counts.international = x.xyz;
+        }
+    })
+    console.log(counts);
+
+    var data = [{
+        values: [counts.domestic, counts.international],
+        labels: ['Domestic', 'International'],
+        type: 'pie'
+      }];
+      
+      var layout = {
+        height: 400,
+        width: 500
+      };
+      
+      Plotly.newPlot('pie', data, layout);
 
 
-// //     // var terrorismData = data.country_txt[0];
-// //     // console.log(terrorismData);
+    });
+
+}
 
 
 
-// // });
+
+
+
 
 
 
